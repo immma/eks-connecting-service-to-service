25ed89fb5a44a6a3b81c132de490867253284394
ardi hanitya <immadiarabdul@gmail.com>
2020-06-25 15:21:50 +0700

    update index.php
    prettify content

--- phpapp/index.php
+++ phpapp/index.php
@@ -1,13 +1,22 @@
 <?php
 
     // $jsonurl = "https://05f73690990b461fb918310c655b6feb.vfs.cloud9.ap-southeast-2.amazonaws.com";
-    $host = 'http://nodeapp-master:8080';
+    $host = 'http://backend.local:8080';
     if (getenv('GET_HOSTS_FROM') == 'env') {
     $host = getenv('NODEAPP_MASTER_SERVICE_HOST');
     }
     
     
     $json = file_get_contents($host);
-    var_dump(json_decode($json));
+    // var_dump(json_decode($json));
+    
+    $jsondata = json_decode($json);
+    
+    foreach ($jsondata as $data) {
+        echo "userID: " . $data->userId . "\n";
+        echo "title: " . $data->title . "\n";
+        echo "comments: " . $data->body . "\n";
+        echo "---------------------------------\n";
+    }
     
 ?>
