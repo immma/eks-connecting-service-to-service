863df2f16974e95145ca9ef3cc4e21378964eb50
immma <immadiarabdul@gmail.com>
2021-08-06 04:53:17 +0700

    update index.php and yaml

---
+++ source/nodeapp/index.js
@@ -0,0 +1,40 @@
+const http = require('http');
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+  res.setHeader('Access-Control-Allow-Methods', "*");
+  res.setHeader('Access-Control-Request-Method', '*');
+
+  res.setHeader('Content-Type', 'application/json');
+
+  var datas = [
+  {
+  "userId": 1,
+  "id": 1,
+  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+  "userId": 1,
+  "id": 2,
+  "title": "qui est esse",
+  "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+  "userId": 1,
+  "id": 3,
+  "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+  "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  }
+];
+  res.end(JSON.stringify(datas));
+});
+
+server.listen(port, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
+});
---
+++ source/phpapp/index.php
@@ -0,0 +1,52 @@
+<?php
+    echo "ENV" . "<br>";
+
+    $host = getenv("NODE_SERVICE");
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+
+    <!-- <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script> -->
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.8.2/angular.min.js"></script>
+    <title>Frontend</title>
+</head>
+
+<body ng-app="serviceApp">
+    <div class="container" ng-controller="serviceCtrl">
+        <div>
+            <?php echo $host; ?>
+
+            <table class="table" ng-repeat="res in response">
+                <tr><td>ID</td><td>{{ res.id }}</td></tr>
+                <tr><td>Title</td><td>{{ res.title }}</td></tr>
+                <tr><td>Body</td><td>{{ res.body }}</td></tr>
+            </table>
+
+        </div>
+    </div>
+
+    <script type="text/javascript">
+        var app = angular.module('serviceApp', []);
+
+        app.controller('serviceCtrl', function($scope, $http) {
+            // $scope.response = [{"id": "21212"}]
+            $http.get("<?php echo $host; ?>").then(function (params) {
+                $scope.response = params.data;
+            }, function error(params) {
+                
+            })
+
+        });
+    </script>
+</body>
+
+</html>
