6c9047d06f8399cbf4af4a5cc611916bd6a56fbd
ardi hanitya <immadiarabdul@gmail.com>
2024-05-10 20:59:08 +0700

    Update index.js

--- nodeapp-v1/index.js
+++ nodeapp-v1/index.js
@@ -11,13 +11,27 @@ const server = http.createServer((req, res) => {
   res.setHeader('Access-Control-Request-Method', '*');
   res.setHeader('Content-Type', 'application/json');
 
-  db.query(sql, function (err, result) {
-    if (err) throw err;
-    // const jsonData = JSON.stringify(result);
-    // console.log(jsonData);
-    // res.end(JSON.stringify(result, null, 2));
-    // return result;
-  });
+  var datas = [
+  {
+  "userId": 1,
+  "id": 1,
+  "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+  "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+  "userId": 1,
+  "id": 2,
+  "title": "qui est esse",
+  "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+  "userId": 1,
+  "id": 3,
+  "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+  "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  }
+];
+  res.end(JSON.stringify(datas));
 });
 
 server.listen(port, () => {
